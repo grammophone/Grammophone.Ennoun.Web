8789142a2f604d0d9ff7f2f9fed0756af60ba2b2
grammophone <emmanuel.liossis@gmail.com>
2016-05-11 02:11:02 +0000

    Fix upper stop and apostrophe normalization.

--- Grammophone.Ennoun.Web/Controllers/TextProcessorStages/CharacterNormalizationStage.cs
+++ Grammophone.Ennoun.Web/Controllers/TextProcessorStages/CharacterNormalizationStage.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 
 namespace Grammophone.Ennoun.Web.Controllers.TextProcessorStages
@@ -14,8 +15,29 @@ namespace Grammophone.Ennoun.Web.Controllers.TextProcessorStages
 		{
 			if (input == null) throw new ArgumentNullException("input");
 
-			return input
-				.Replace('˙', '·'); // Correct the type of upper stop.
+			var stringBuilder = new StringBuilder(input.Length);
+
+			for (int i = 0; i < input.Length; i++)
+			{
+				char c = input[i];
+
+				switch (c)
+				{
+					case '˙': // Correct the type of upper stop.
+					case '\x00B7':
+						c = '·';
+						break;
+
+					case '\'': // Correct the type of apostrophe.
+					case '\x2019':
+						c = '\x1FBF';
+						break;
+				}
+
+				stringBuilder.Append(c);
+			}
+
+			return stringBuilder.ToString();
 		}
 	}
 }
